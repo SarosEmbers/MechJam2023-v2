a069b6351fa518909cabc5659dd4b7bc54a27698
Elijah Anderson <ejanderson00@gmail.com>
2023-07-15 02:13:49 -0400

    Push for Joshie <3

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerAim : MonoBehaviour
 {
+    public Transform reticle;
+    public float maxRange = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward * maxRange);
     }
 }
