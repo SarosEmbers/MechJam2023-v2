6ad36fab0a99a2259e617b64510bedf1f70111e9
Elijah Anderson <ejanderson00@gmail.com>
2023-07-17 22:00:40 -0400

    Revert "Reimporting models and textures"
    This reverts commit d8c72fee4e224afc611a9b8aa000256ff8770f96.

--- Assets/playerAimAttack.cs
+++ Assets/playerAimAttack.cs
@@ -83,10 +83,6 @@ public class playerAimAttack : MonoBehaviour
                 }
                 else
                 {
-                    RaycastHit farHit;
-                    Physics.Raycast(reticlePos.position, reticlePos.forward, out farHit, maxLockOnDistance);
-                    Vector3 closestPoint = GetComponent<Collider>().ClosestPoint(transform.position);
-
                     Quaternion simplyForward = Quaternion.LookRotation(orientation.forward);
                     PLArm.transform.rotation = simplyForward;
                     //PLArm.transform.rotation = Quaternion.Lerp(PLArm.transform.rotation, simplyForward, 15 * Time.deltaTime);
